16388ac666ea084ea265b3908364e23ed67cee7d
Yanstime <862415702@qq.com>
2019-06-18 16:16:47 +0800

    Initial commit

---
+++ v99/serviceworker.js
@@ -0,0 +1,25 @@
+self.addEventListener("install", (e) => {});
+self.addEventListener("activate", (e) => {});
+self.addEventListener("fetch", (e) => {});
+
+/*
+     FILE ARCHIVED ON 19:38:11 Mar 31, 2019 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 07:31:47 Jun 18, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 61.443 (3)
+  esindex: 0.013
+  captures_list: 83.132
+  CDXLines.iter: 13.141 (3)
+  PetaboxLoader3.datanode: 63.011 (4)
+  exclusion.robots: 0.18
+  exclusion.robots.policy: 0.168
+  RedisCDXSource: 5.262
+  PetaboxLoader3.resolve: 73.346
+  load_resource: 102.136
+*/
